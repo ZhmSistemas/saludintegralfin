3e58be37e121d831995a0b4def3647a276fcb6a0
willy <zhmsistemas@gmail.com>
2026-08-10 17:52:03 -0500

    agrega imagenes en mis facturasd

--- src/components/ListaFacturas.tsx
+++ src/components/ListaFacturas.tsx
@@ -598,7 +598,7 @@ export default function ListaFacturas() {
         >
           <button
             onClick={() => setSelectedImage(null)}
-            className="absolute right-4 top-4 rounded-full bg-white/20 p-2 text-white transition-colors hover:bg-white/40"
+            className="absolute right-4 top-4 z-[60] rounded-full bg-white/20 p-2 text-white transition-colors hover:bg-white/40"
           >
             <X className="h-6 w-6" />
           </button>
@@ -607,7 +607,7 @@ export default function ListaFacturas() {
             alt="Factura ampliada"
             width={1200}
             height={1600}
-            className="max-h-[90vh] max-w-[90vw] rounded-lg object-contain shadow-2xl"
+            className="relative z-10 max-h-[90vh] max-w-[90vw] rounded-lg object-contain shadow-2xl"
             onClick={(e) => e.stopPropagation()}
           />
         </div>
